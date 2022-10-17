09c696b061001769e1bd863794530b56071fa363
NikitaNazarov179 <99790237+NikitaNazarov179@users.noreply.github.com>
2022-10-17 23:10:07 +0300

    fixed ind

--- Задания/ind.py
+++ Задания/ind.py
@@ -5,39 +5,30 @@ import sys
 
 if __name__ == '__main__':
     print("help - список всех команд")
-    # Список товаров.
     shops = []
 
-    # Организовать бесконечный цикл запроса команд.
     while True:
-        # Запросить команду из терминала.
         command = input(">>> ").lower()
 
-        # Выполнить действие в соответствие с командой.
         if command == 'exit':
             break
 
         elif command == 'add':
-            # Запросить данные о человеке.
             product = input("Название продукта: ")
             magazin = input("Магазин: ")
             cost = input("Стоимость товара: ")
 
-            # Создать словарь.
             shop = {
                 'product': product,
                 'magazin': magazin,
                 'cost': cost,
             }
 
-            # Добавить словарь в список.
             shops.append(shop)
-            # Отсортировать список в случае необходимости.
             if len(shops) > 1:
                 shops.sort(key=lambda d: d.get('product', ''))
 
         elif command == 'list':
-            # Заголовок таблицы.
             line = '+-{}-+-{}-+-{}-+-{}-+'.format(
                 '-' * 4,
                 '-' * 30,
@@ -55,7 +46,6 @@ if __name__ == '__main__':
             )
             print(line)
 
-            # Вывести данные о всех товарах.
             for idx, shtuka in enumerate(shops, 1):
                 print(
                     '| {:^4} | {:<30} | {:<20} | {:<15} |'.format(
@@ -70,12 +60,9 @@ if __name__ == '__main__':
             print(line)
 
         elif command.startswith('select '):
-            # Получить введенный товар
             addedtovar = command[7:]
 
-            # Инициализировать счетчик.
             count = 0
-            # Проверить сведения работников из списка.
             for shop in shops:
                 if shop.get('product', '') == addedtovar:
                     count += 1
@@ -86,17 +73,16 @@ if __name__ == '__main__':
                         )
                     )
 
-            # Если счетчик равен 0, то работники не найдены.
             if count == 0:
                 print("Данный товар не найден.")
 
         elif command == 'help':
-            # Вывести справку о работе с программой.
             print("Список команд:\n")
             print("add - добавить товар;")
             print("list - вывести список товаров;")
             print("help - список всех команд;")
             print("exit - завершить работу с программой.")
+            print("select... - Получить информацию об определенном товаре")
 
         else:
             print(f"Неизвестная команда {command}", file=sys.stderr)
